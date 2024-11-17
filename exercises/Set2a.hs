29ef3013267fa0a1789accbb947f8776ac560abd
giuliozabotto <giulio.zabotto@serinf.it>
2024-11-17 10:33:00 +0100

    completes set2a

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -142,7 +142,10 @@ greet first Nothing = "Hello, " ++ first ++ "!"
 --   safeIndex ["a","b","c"] (-1)  ==> Nothing
 
 safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i = todo
+safeIndex xs i = 
+    if i < length xs && i >= 0
+    then Just(last(take (i+1) xs))
+    else Nothing
 
 ------------------------------------------------------------------------------
 -- Ex 10: another variant of safe division. This time you should use
@@ -153,7 +156,14 @@ safeIndex xs i = todo
 --   eitherDiv 4 0   ==> Left "4/0"
 
 eitherDiv :: Integer -> Integer -> Either String Integer
-eitherDiv x y = todo
+eitherDiv x y 
+    | y == 0 = Left( show x ++ "/0")
+    | otherwise = Right( div x y )
+    -- if y == 0
+    -- then Left ("Error: division by zero")
+    -- else Right( x/y )
+-- eitherDiv x 0 = Left ("Error: division by zero")
+-- eitherDiv x y = Right (x / y)
 
 ------------------------------------------------------------------------------
 -- Ex 11: implement the function addEithers, which combines two values of type
@@ -170,4 +180,7 @@ eitherDiv x y = todo
 --   addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+-- addEithers 
+addEithers (Right x) (Right y) = Right(x+y)
+addEithers (Right x) (Left y) = Left y
+addEithers (Left x) _ = Left x 
